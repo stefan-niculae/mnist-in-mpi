2d9894f063a1b2b29e8619af797d0bf29d881959
Stefan Niculae <stefan1niculae@gmail.com>
2017-02-04 21:29:41 +0200

    Add read images function (not working)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <vector>
 #include "utils.cpp"
 
 using namespace std;
 
 
 int main() {
-    vector<int> v0 = {0, 2, 3};
-    vector<int> v1 = {1, 5, 6};
-    vector<int> v2 = {2, 0, 1};
-
-    vector<vector<int>> m = {v0, v1, v2};
-
-    print(m * m);
+    Matrix m = read_images("/Users/Stefan/Downloads/train_images", 10000, 28 * 28);
+    print(m[0]);
+    print(m[1]);
 }
