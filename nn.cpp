c236e4d023084887207c6df5b68c3aaf248f6309
Stefan Niculae <stefan1niculae@gmail.com>
2017-02-05 13:02:05 +0200

    Fixes softmax

--- nn.cpp
+++ nn.cpp
@@ -18,14 +18,13 @@ Vector softmax(const Vector& v) {
     }
 
     Vector result(v.size(), 0.);
-    for (double x : expd) {
-        result.push_back(x / sum);
-    }
+    for (int i = 0; i < v.size(); ++i)
+        result[i] = expd[i] / sum;
 
     return result;
 }
 Matrix softmax(const Matrix& m) {
-    auto result = Matrix(m);
+    Matrix result = m;
     for (auto& row : result)
         row = softmax(row);
     return result;
@@ -55,20 +54,20 @@ public:
     }
 
     double grad(Matrix X, Matrix Y, Matrix &grad_W, Vector &grad_b) {
-        cout << "X dims: " << n_rows(X) << " " << n_cols(X) << endl;
-        cout << "W dims: " << n_rows(W) << " " << n_cols(W) << endl;
+//        cout << "X dims: " << n_rows(X) << " " << n_cols(X) << endl;
+//        cout << "W dims: " << n_rows(W) << " " << n_cols(W) << endl;
 
-        cout << "Result: " << n_rows(X * W + b) << " " << n_cols(X * W + b) << endl;
+//        cout << "Result: " << n_rows(X * W + b) << " " << n_cols(X * W + b) << endl;
 
         Matrix Y_prob = softmax(X * W + b);
 
-        cout << "Result Softmax: " << n_rows(Y_prob) << " " << n_cols(Y_prob) << endl;
+//        cout << "Result Softmax: " << n_rows(Y_prob) << " " << n_cols(Y_prob) << endl;
 
         // error at last layer
         Matrix delta = Y_prob - Y;
 
         // return gradient of cross entropy cost
-        grad_W = delta * X;
+        grad_W = transpose(X) * delta;
         // grad_b = delta;
         return cross_entropy(Y, Y_prob);
     }
@@ -79,11 +78,11 @@ public:
         Matrix grad_W = blank_matrix(this->data_dim, this->n_classes, 0.);
         Vector grad_b;
 
-        for(int epoch=0; epoch<epochs; epoch++) {
-            for(int i=0; i<n_rows(X); i+=batch_size) {
+        for (int epoch=0; epoch<epochs; epoch++) {
+            for (int i=0; i<n_rows(X); i+=batch_size) {
 
                 auto cost = grad(chunk(X, i, i+batch_size), chunk(Y, i, i+batch_size), grad_W, grad_b);
-
+                cout << "epoch " << epoch << ", cost = " << cost << endl;
                 W = W - lr * 1/batch_size * grad_W;
                 // b = b - lr * 1/batch_size * grad_b;
 
